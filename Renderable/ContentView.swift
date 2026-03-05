401e568d713f34621352332e8ee90d8d8aabb6a4
kasper.m9 <kaspermuutra@gmail.com>
2026-03-05 16:56:24 +0200

    Initial Commit

---
+++ Renderable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Renderable
+//
+//  Created by Kasper Muutra on 05.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Renderable/RenderableApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenderableApp.swift
+//  Renderable
+//
+//  Created by Kasper Muutra on 05.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct RenderableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
