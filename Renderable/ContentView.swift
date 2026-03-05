9681c822f1ec4eb6d22b13c5108ac49aca46b401
kasper.m9 <kaspermuutra@gmail.com>
2026-03-05 21:04:56 +0200

    Add SwiftUI home screen and RoomPlan scanner setup

--- Renderable/ContentView.swift
+++ Renderable/ContentView.swift
@@ -1,24 +1,34 @@
-//
-//  ContentView.swift
-//  Renderable
-//
-//  Created by Kasper Muutra on 05.03.2026.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
 
-#Preview {
-    ContentView()
+    @State private var showScanner = false
+
+    var body: some View {
+        ZStack {
+            Color.black.ignoresSafeArea()
+
+            VStack(spacing: 40) {
+                Text("Renderable")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+
+                Button(action: {
+                    showScanner = true
+                }) {
+                    Text("Scan Room")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.black)
+                        .frame(width: 220, height: 60)
+                        .background(Color.white)
+                        .cornerRadius(30)
+                }
+            }
+        }
+        .fullScreenCover(isPresented: $showScanner) {
+            RoomScannerView()
+        }
+    }
 }
